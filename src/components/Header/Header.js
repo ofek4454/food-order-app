13c653ef128c0ececc366433ed936bacaae41e78
ofek4454 <ofek4454@gmail.com>
2023-05-04 15:04:47 +0300

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import img from "../../assets/meals.jpg";
+import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={img} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
